18ea13db379c7c9099436f9532f28745898bad3a
Takashi UNO <a99s504@auone.jp>
2011-04-30 01:56:28 +0900

    modify date format of file name.

--- hibiki.sh
+++ hibiki.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# hibiki.sh ver.0.2 (2011-04-25)
+# hibiki.sh ver.0.2.1 (2011-04-30)
 # require wget, ruby, nkf and mimms
 
 if [ $# -eq 1 ]; then
@@ -9,7 +9,7 @@ if [ $# -eq 1 ]; then
   # save asx file
   wget -q -O - http://hibiki-radio.jp/description/${STR} | grep movie | ruby -ruri -e 'puts URI.extract(ARGF.read, "http")' | head -1 | xargs wget -q -O - | grep asx | ruby -ruri -e 'puts URI.extract(ARGF.read, "http")' | uniq | xargs wget -q -O - | nkf -w >${TMPFILE}
 
-  TITLE=`cat ${TMPFILE} | ruby -rrexml/document -e 'puts REXML::Document.new(ARGF).elements["ASX/TITLE"].text' | tr '/' '-'`
+  TITLE=`cat ${TMPFILE} | ruby -rrexml/document -e 'puts REXML::Document.new(ARGF).elements["ASX/TITLE"].text' | tr '/' '月' | sed -e "s/ WMV/日配信/"`
   WMVFILE=`cat ${TMPFILE} | ruby -rrexml/document -e 'puts REXML::Document.new(ARGF).elements["ASX/ENTRY/REF"].attributes["HREF"]'`
 
   mimms ${WMVFILE} "${TITLE}.asf"
