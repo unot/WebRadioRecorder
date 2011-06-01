2737a04fd16a2fda213ec993ecb8df7951f200bd
Takashi UNO <a99s504@auone.jp>
2011-06-02 03:05:36 +0900

    hibiki.sh: apply changes of asx format.

--- hibiki.sh
+++ hibiki.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# hibiki.sh ver.0.2.3 (2011-05-19)
+# hibiki.sh ver.0.3 (2011-06-01)
 # require wget, ruby, nkf and mimms
 
 if [ $# -eq 1 ]; then
@@ -7,16 +7,17 @@ if [ $# -eq 1 ]; then
   TMPFILE="/var/tmp/tmp.$$"
 
   # save asx file
-  wget -q -O - http://hibiki-radio.jp/description/${STR} | grep movie | ruby -ruri -e 'puts URI.extract(ARGF.read, "http")' | head -1 | xargs wget -q -O - | grep asx | ruby -ruri -e 'puts URI.extract(ARGF.read, "http")' | uniq | xargs wget -q -O - | nkf -w >${TMPFILE}
+  wget -q -O - http://hibiki-radio.jp/description/${STR} | grep movie | ruby -ruri -e 'puts URI.extract(ARGF.read, "http")' | head -1 | xargs wget -q -O ${TMPFILE}
 
-  TITLE=`cat ${TMPFILE} | ruby -rrexml/document -e 'puts REXML::Document.new(ARGF).elements["ASX/TITLE"].text' | tr '/' '月'`日配信
-  WMVFILE=`cat ${TMPFILE} | ruby -rrexml/document -e 'puts REXML::Document.new(ARGF).elements["ASX/ENTRY/REF"].attributes["HREF"]'`
-  if test "$TITLE" = "" ; then
+  WMVFILE=`cat ${TMPFILE} | ruby -ruri -e 'puts URI.extract(ARGF.read, "mms")' | uniq`
+  TITLE=`cat ${TMPFILE} | head -n +58 | tail -n 1 | sed -e 's/^ *//g; s/<br \/>//g; s/\//月/'| tr -d '\015'`
+  if test a"$WMVFILE" = a"" ; then
     echo "asx detection failed."
+    rm ${TMPFILE}
     exit 1
   fi
 
-  mimms ${WMVFILE} "${TITLE}.asf"
+  mimms ${WMVFILE} "${TITLE}日配信.asf"
   rm ${TMPFILE}
   exit 0
 else
