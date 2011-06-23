d9283914e55ced65edd3da13996cd3ce1e00e824
Takashi UNO <a99s504@auone.jp>
2011-06-23 23:19:56 +0900

    hibiki.sh: support asx format variation.

--- hibiki.sh
+++ hibiki.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# hibiki.sh ver.0.2.3 (2011-05-19)
+# hibiki.sh ver.0.3.1 (2011-06-12)
 # require wget, ruby, nkf and mimms
 
 if [ $# -eq 1 ]; then
@@ -11,9 +11,15 @@ if [ $# -eq 1 ]; then
 
   TITLE=`cat ${TMPFILE} | ruby -rrexml/document -e 'puts REXML::Document.new(ARGF).elements["ASX/TITLE"].text' | tr '/' '月'`日配信
   WMVFILE=`cat ${TMPFILE} | ruby -rrexml/document -e 'puts REXML::Document.new(ARGF).elements["ASX/ENTRY/REF"].attributes["HREF"]'`
-  if test "$TITLE" = "" ; then
-    echo "asx detection failed."
-    exit 1
+  if test a"$TITLE" = a"" ; then
+    wget -q -O - http://hibiki-radio.jp/description/${STR} | grep movie | ruby -ruri -e 'puts URI.extract(ARGF.read, "http")' | head -1 | xargs wget -q -O ${TMPFILE}
+    TITLE=`cat ${TMPFILE} | head -n +58 | tail -n 1 | sed -e 's/^ *//g; s/<br \/>//g; s/\//月/' | tr -d '\015'`
+    WMVFILE=`cat ${TMPFILE} | ruby -ruri -e 'puts URI.extract(ARGF.read, "mms")' | uniq`
+    if test b"$TITLE" = b"" ; then
+	echo "asx detection failed."
+	rm ${TMPFILE}
+	exit 1
+    fi
   fi
 
   mimms ${WMVFILE} "${TITLE}.asf"
