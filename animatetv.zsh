4d96ed1f16a86ee5d446e3a85d00e0d401d4ed3d
Takashi UNO <a99s504@auone.jp>
2012-10-20 01:05:23 +0900

    modify animatetv.zsh

--- animatetv.zsh
+++ animatetv.zsh
@@ -1,5 +1,5 @@
-#!/bin/zsh
-# animatetv.zsh version 0.4 (2011-05-01)
+#!/bin/zsh -e
+# animatetv.zsh version 0.4.1 (2012-10-20)
 # require wget, perl, nkf and mimms
 
 TMPFILE="/var/tmp/tmp.$$"
@@ -8,7 +8,11 @@ RMOPT=
 if [ $# = 1 ]; then
   REFID=$1
   ASX=http://www.animate.tv`wget -q http://animate.tv/radio/details.php\?id\=${REFID} -O - | nkf -w | grep play.php | head -1 | perl -nle 'm|(/play.*player)|; print $1'`
-  wget -q --referer="http://animate.tv/radio/details.php\?id=${REFID}" --user-agent='Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)' ${ASX} -O - | nkf -w >${TMPFILE}
+  wget -q --referer="http://animate.tv/radio/details.php\?id=${REFID}" --user-agent='Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)' ${ASX} -O ${TMPFILE} #- | nkf -w >${TMPFILE}
+  if [ $? -ne 0 -o ! -s ${TMPFILE} ]; then
+    echo "ERROR download ${ASX}"
+    exit 1
+  fi
   TITLE=`cat ${TMPFILE} | perl -nle 'm|<Entry><Title>(.*)</Title>|; print $1' | nkf -w`
   MMS=`cat ${TMPFILE} | perl -nle 'm|(mms.*wma)|; print $1'`
   if [ "${MMS}" = "" ]; then
