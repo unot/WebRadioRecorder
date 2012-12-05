234d0ce802568f9215378e8544dc712e5a52eef6
Takashi UNO <a99s504@auone.jp>
2012-12-06 06:29:05 +0900

    add flv2m4a.sh

---
+++ flv2m4a.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+if [ $# -ne 1 ]; then
+	echo "USAGE: `basename $0` hogehoge.flv"
+	exit 1
+fi
+
+FFMPEG=/usr/local/bin/ffmpeg
+AFCONVERT=/usr/bin/afconvert
+FLVFILE=$1
+AACFILE=${FLVFILE%.flv}.aac
+trap 'rm -f ${AACFILE}' EXIT
+
+${FFMPEG} -i ${FLVFILE} -vn -acodec copy ${AACFILE}
+if [ $? != 0 ]; then
+    # http://bui.asablo.jp/blog/2012/02/24/6347167
+    ${FFMPEG} -i ${FLVFILE} ${AACFILE}
+fi
+${AFCONVERT} -f m4af -d aach -b 48000 ${AACFILE}
+exit 0
