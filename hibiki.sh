0867902e4214a31a7d6b8a9ff5297ff814c0c3e6
Takashi UNO <a99s504@auone.jp>
2013-01-31 23:02:51 +0900

    modify hibiki.sh

--- hibiki.sh
+++ hibiki.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
-# hibiki.sh ver.0.4 (2012-12-24)
-# require wget, ruby, nkf and mimms
+# hibiki.sh ver.0.5.3 (2013-01-31)
+# require wget, ruby, and mimms
 TMPFILE="/var/tmp/tmp.$$"
 trap 'rm -f ${TMPFILE}' EXIT
 
@@ -8,21 +8,16 @@ if [ $# -eq 1 ]; then
   STR=$1
 
   # save asx file
-  wget -q -O - http://hibiki-radio.jp/description/${STR} | grep movie | ruby -ruri -e 'puts URI.extract(ARGF.read, "http")' | head -1 | xargs wget -q -O - | grep asx | ruby -ruri -e 'puts URI.extract(ARGF.read, "http")' | uniq | xargs wget -q -O - | nkf -w >${TMPFILE}
+  wget -q -O - http://hibiki-radio.jp/description/${STR} | grep WMV | ruby -ruri -e 'puts URI.extract(ARGF.read, "http")' | head -1 | xargs wget -q -O ${TMPFILE}
   if [ -z ${TMPFILE} ]; then
     echo "download ERROR"
     exit 1
   fi
-  TITLE=`cat ${TMPFILE} | ruby -rrexml/document -e 'puts REXML::Document.new(ARGF).elements["ASX/TITLE"].text' | tr '/' '-'`
-  WMVFILE=`cat ${TMPFILE} | ruby -rrexml/document -e 'puts REXML::Document.new(ARGF).elements["ASX/ENTRY/REF"].attributes["HREF"]'`
+  TITLE=`cat ${TMPFILE} | ruby -rrexml/document -e 'puts REXML::Document.new(ARGF).elements["ASX/entry/title"].text'`
+  WMVFILE=`cat ${TMPFILE} | ruby -rrexml/document -e 'puts REXML::Document.new(ARGF).elements["ASX/entry/Ref"].attributes["href"]'`
   if test a"$TITLE" = a"" ; then
-    wget -q -O - http://hibiki-radio.jp/description/${STR} | grep movie | ruby -ruri -e 'puts URI.extract(ARGF.read, "http")' | head -1 | xargs wget -q -O ${TMPFILE}
-    TITLE=`cat ${TMPFILE} | head -n +58 | tail -n 1 | sed -e 's/^ *//g; s/<br \/>//g; s/\//月/' | tr -d '\015'`
-    WMVFILE=`cat ${TMPFILE} | ruby -ruri -e 'puts URI.extract(ARGF.read, "mms")' | uniq`
-    if test b"$TITLE" = b"" ; then
-      echo "asx detection failed."
-      exit 1
-    fi
+    echo "asx detection failed."
+    exit 1
   fi
 
   mimms ${WMVFILE} "${TITLE}.asf"
