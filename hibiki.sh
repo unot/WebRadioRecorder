3f49a249283c16ea15cf71c0d975b1c4ec660b7d
Takashi UNO <a99s504@auone.jp>
2013-02-27 06:37:41 +0900

    hibiki.sh, rec_aandgplus.sh: change the recording method to retry.

--- hibiki.sh
+++ hibiki.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-# hibiki.sh ver.0.5.5 (2013-02-05)
+# hibiki.sh ver.0.5.6 (2013-02-13)
 # require wget, ruby, and mimms
 TMPFILE="/var/tmp/tmp.$$"
 trap 'rm -f ${TMPFILE}' EXIT
@@ -8,7 +8,7 @@ if [ $# -eq 1 ]; then
   STR=$1
 
   # save asx file
-  wget -q -O - http://hibiki-radio.jp/description/${STR} | grep WMV | ruby -ruri -e 'puts URI.extract(ARGF.read, "http")' | head -1 | xargs wget -q -O ${TMPFILE}
+  wget -q -O - http://hibiki-radio.jp/description/${STR} | grep WMV | ruby -ruri -e 'puts URI.extract(ARGF.read, "http")' | tail -1 | xargs wget -q -O ${TMPFILE}
   if [ -z ${TMPFILE} ]; then
     echo "download ERROR"
     exit 1
@@ -20,6 +20,7 @@ if [ $# -eq 1 ]; then
     exit 1
   fi
 
+  mimms -r ${WMVFILE} "${TITLE}.asf" || mimms -r ${WMVFILE} "${TITLE}.asf"
   mimms -r ${WMVFILE} "${TITLE}.asf" || mimms -r ${WMVFILE} "${TITLE}.asf"
 
 else
--- rec_aandgplus.sh
+++ rec_aandgplus.sh
@@ -11,11 +11,14 @@ OUTFILE="${DESTDIR}/${TITLE}_${XDATE}.flv"
 STOPMINS=${2:-30}
 STOPSECS=`expr ${STOPMINS} \* 60 + 120`
 
-${RTMPDUMP} --rtmp "rtmpe://fms1.uniqueradio.jp/" \
-	--playpath "aandg2" \
+for i in 5 4 3 2 1 ; do
+	${RTMPDUMP} --rtmp "rtmpe://fms1.uniqueradio.jp/" \
+	--playpath "aandg$i" \
 	--app "?rtmp://fms-base1.mitene.ad.jp/agqr/" \
 	--stop ${STOPSECS} \
 	--live \
 	-o "${OUTFILE}"
-
-exit 0
+	if [ $? -eq 0 ]; then
+		break;
+	fi
+done
