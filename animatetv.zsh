8132ea959f6862ba4f4a77bb1d8c5c962ea8641a
Takashi UNO <a99s504@auone.jp>
2013-09-26 05:30:48 +0900

    modify animatetv.zsh and flv2m4a.sh
    animatetv.zsh: add cookie options
    flv2m4a.sh: add doublequote

--- animatetv.zsh
+++ animatetv.zsh
@@ -1,20 +1,21 @@
-#!/bin/zsh -e
+#!/bin/zsh -ex
 # animatetv.zsh version 0.4.1 (2012-10-20)
 # require wget, perl, nkf and mimms
 
 TMPFILE="/var/tmp/tmp.$$"
 RMOPT=
+trap 'rm -rf "${TMPFILE}" "${TMPFILE}_cookie.txt"' EXIT
 
 if [ $# = 1 ]; then
   REFID=$1
-  ASX=http://www.animate.tv`wget -q http://animate.tv/radio/details.php\?id\=${REFID} -O - | nkf -w | grep play.php | head -1 | perl -nle 'm|(/play.*?player)|; print $1'`
-  wget -q --referer="http://animate.tv/radio/details.php\?id=${REFID}" --user-agent='Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)' ${ASX} -O ${TMPFILE} #- | nkf -w >${TMPFILE}
+  ASX=http://www.animate.tv`wget -q http://animate.tv/radio/details.php\?id\=${REFID} -O - --save-cookies=${TMPFILE}_cookie.txt --keep-session-cookies | nkf -w | grep play.php | head -1 | perl -nle 'm|(/play.*?player)|; print $1'`
+  wget -q --referer="http://animate.tv/radio/details.php\?id=${REFID}" --user-agent='Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)' --load-cookies=${TMPFILE}_cookie.txt ${ASX} -O ${TMPFILE} #- | nkf -w >${TMPFILE}
   if [ $? -ne 0 -o ! -s ${TMPFILE} ]; then
     echo "ERROR download ${ASX}"
     exit 1
   fi
   TITLE=`cat ${TMPFILE} | perl -nle 'm|<Entry><Title>(.*)</Title>|; print $1' | nkf -w`
-  MMS=`cat ${TMPFILE} | perl -nle 'm|(mms.*wma)|; print $1'`
+  MMS=`cat ${TMPFILE} | perl -nle 'm|(mms.*?wma)|; print $1'`
   if [ "${MMS}" = "" ]; then
     echo "failed to get mms url."
     rm ${TMPFILE}
@@ -24,7 +25,6 @@ if [ $# = 1 ]; then
 #  echo ${MMS}
 #  echo "${TITLE}.wma"
 #  : >"${TITLE}.wma"
-  rm ${RMOPT} ${TMPFILE}
 else
   echo "usage: `basename $0` hoge"
 fi
--- flv2m4a.sh
+++ flv2m4a.sh
@@ -6,14 +6,14 @@ fi
 
 FFMPEG=/usr/local/bin/ffmpeg
 AFCONVERT=/usr/bin/afconvert
-FLVFILE=$1
-AACFILE=${FLVFILE%.flv}.aac
+FLVFILE="$1"
+AACFILE="${FLVFILE%.???}.aac"
 trap 'rm -f ${AACFILE}' EXIT
 
-${FFMPEG} -i ${FLVFILE} -vn -acodec copy ${AACFILE}
+${FFMPEG} -i "${FLVFILE}" -vn -acodec copy "${AACFILE}"
 if [ $? != 0 ]; then
     # http://bui.asablo.jp/blog/2012/02/24/6347167
-    ${FFMPEG} -i ${FLVFILE} ${AACFILE}
+    ${FFMPEG} -i "${FLVFILE}" "${AACFILE}"
 fi
-${AFCONVERT} -f m4af -d aach -b 48000 ${AACFILE}
+${AFCONVERT} -f m4af -d aach -b 48000 "${AACFILE}"
 exit 0
