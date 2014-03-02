1987507977b816b733e5e2dbd1a4b2ce71d6458c
Takashi UNO <a99s504@auone.jp>
2014-03-02 20:11:29 +0900

    modify animatetv.zsh
    avoid a link of flash.

--- animatetv.zsh
+++ animatetv.zsh
@@ -1,4 +1,4 @@
-#!/bin/zsh -ex
+#!/bin/zsh -e
 # animatetv.zsh version 0.4.1 (2012-10-20)
 # require wget, perl, nkf and mimms
 
@@ -8,7 +8,7 @@ trap 'rm -rf "${TMPFILE}" "${TMPFILE}_cookie.txt"' EXIT
 
 if [ $# = 1 ]; then
   REFID=$1
-  ASX=http://www.animate.tv`wget -q http://animate.tv/radio/details.php\?id\=${REFID} -O - --save-cookies=${TMPFILE}_cookie.txt --keep-session-cookies | nkf -w | grep play.php | head -1 | perl -nle 'm|(/play.*?player)|; print $1'`
+  ASX=http://www.animate.tv`wget -q http://animate.tv/radio/details.php\?id\=${REFID} -O - --save-cookies=${TMPFILE}_cookie.txt --keep-session-cookies | nkf -w | grep play.php | gsed '1d' | perl -nle 'm|(/play.*?player)|; print $1'`
   wget -q --referer="http://animate.tv/radio/details.php\?id=${REFID}" --user-agent='Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)' --load-cookies=${TMPFILE}_cookie.txt ${ASX} -O ${TMPFILE} #- | nkf -w >${TMPFILE}
   if [ $? -ne 0 -o ! -s ${TMPFILE} ]; then
     echo "ERROR download ${ASX}"
